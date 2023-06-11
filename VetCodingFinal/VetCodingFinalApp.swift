287796db951f34882b88c9b65758656ba60d3146
mattwbdv <koenigmattd@gmail.com>
2023-06-11 16:42:14 -0700

    Initial Commit

---
+++ VetCodingFinal/VetCodingFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VetCodingFinalApp.swift
+//  VetCodingFinal
+//
+//  Created by Matt Koenig on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VetCodingFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
